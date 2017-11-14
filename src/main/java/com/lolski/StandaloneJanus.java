1e56a4392daf9d20762e59848db91338311be32f
Ganeshwara Herawan Hananda <lolski@Ganeshwaras-MacBook-Pro.local>
2017-11-14 19:20:50 +0000

    standalone janus wip

--- src/main/java/com/lolski/StandaloneJanus.java
+++ src/main/java/com/lolski/StandaloneJanus.java
@@ -12,6 +12,7 @@ import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.util.GraphFactory;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
+import org.janusgraph.core.JanusGraphTransaction;
 import org.janusgraph.example.GraphOfTheGodsFactory;
 
 import java.util.HashMap;
@@ -38,35 +39,35 @@ public class StandaloneJanus {
 //        map.put(AppConstants.STORAGE_CASSANDRA_KEYSPACE, keyspace);
 //        map.put(AppConstants.GREMLIN_GRAPH, AppConstants.GREMLIN_GRAPH_VALUE_HADOOP);
 //        map.put(Constants.GREMLIN_HADOOP_INPUT_LOCATION, null);
-        
+
         map.put("cassandra.input.keyspace", keyspace);
-        map.put("gremlin.hadoop.jarsInDistributedCache", true);
-        map.put("cassandra.input.predicate", "0c00020b0001000000000b000200000000020003000800047fffffff0000");
-        map.put("janusgraphmr.ioformat.cf-name", "edgestore");
-        map.put("janusgraphmr.ioformat.conf.storage.hostname", "localhost");
-        map.put("cassandra.thrift.framed.size_mb", 1024);
+//        map.put("gremlin.hadoop.jarsInDistributedCache", true);
+//        map.put("cassandra.input.predicate", "0c00020b0001000000000b000200000000020003000800047fffffff0000");
+//        map.put("janusgraphmr.ioformat.cf-name", "edgestore");
+//        map.put("janusgraphmr.ioformat.conf.storage.hostname", "localhost");
+//        map.put("cassandra.thrift.framed.size_mb", 1024);
         map.put("janusgraphmr.ioformat.conf.storage.cassandra.keyspace", keyspace);
-        map.put("cassandra.input.partitioner.class", "org.apache.cassandra.dht.Murmur3Partitioner");
-        map.put("gremlin.hadoop.outputLocation", "output/1342398146");
-        map.put("janusmr.ioformat.conf.storage.backend", "cassandra");
+//        map.put("cassandra.input.partitioner.class", "org.apache.cassandra.dht.Murmur3Partitioner");
+//        map.put("gremlin.hadoop.outputLocation", AppConstants.GREMLIN_HADOOP_OUTPUT_LOCATION_VALUE_STANDALONE_SPARK);
+//        map.put("janusmr.ioformat.conf.storage.backend", "cassandra");
         map.put("janusmr.ioformat.cf-name", "edgestore");
         map.put("cassandra.input.columnfamily", "edgestore");
         map.put("janusmr.ioformat.conf.storage.cassandra.keyspace", keyspace);
-        map.put("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+//        map.put("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
         map.put("gremlin.graph", "org.apache.tinkerpop.gremlin.hadoop.structure.HadoopGraph");
         map.put("gremlin.hadoop.graphWriter", "org.apache.tinkerpop.gremlin.hadoop.structure.io.gryo.GryoOutputFormat");
         map.put("storage.hostname", "localhost");
         map.put("janusgraphmr.ioformat.conf.storage.backend", "cassandra");
         map.put("janusmr.ioformat.conf.storage.hostname", "localhost");
-        map.put("gremlin.hadoop.graphReader", "org.janusgraph.hadoop.formats.cassandra.CassandraInputFormat");
-        map.put("gremlin.hadoop.inputLocation", "none");
+//        map.put("gremlin.hadoop.graphReader", "org.janusgraph.hadoop.formats.cassandra.CassandraInputFormat");
+//        map.put("gremlin.hadoop.inputLocation", "none");
         map.put(AppConstants.STORAGE_BACKEND, "cassandra");
 
         return map;
     }
 
     public static Map<String, Object> newJanusHadoopConfiguration(Map<String, Object> configuration) {
-        Map<String, Object> newConfiguration = new HashMap<String, Object>(configuration);
+        Map<String, Object> newConfiguration = new HashMap<>(configuration);
         newConfiguration.put(AppConstants.JANUSMR_IOFORMAT_CONF_STORAGE_CASSANDRA_KEYSPACE, keyspace);
         return newConfiguration;
     }
@@ -77,14 +78,14 @@ public class StandaloneJanus {
         computer.configure(AppConstants.SPARK_SERIALIZER, AppConstants.SPARK_SERIALIZER_VALUE);
 
         computer.configure("cassandra.input.keyspace", keyspace);
-        computer.configure("gremlin.hadoop.jarsInDistributedCache", true);
-        computer.configure("cassandra.input.predicate", "0c00020b0001000000000b000200000000020003000800047fffffff0000");
-        computer.configure("janusgraphmr.ioformat.cf-name", "edgestore");
+//        computer.configure("gremlin.hadoop.jarsInDistributedCache", true);
+//        computer.configure("cassandra.input.predicate", "0c00020b0001000000000b000200000000020003000800047fffffff0000");
+//        computer.configure("janusgraphmr.ioformat.cf-name", "edgestore");
         computer.configure("janusgraphmr.ioformat.conf.storage.hostname", "localhost");
-        computer.configure("cassandra.thrift.framed.size_mb", 1024);
+//        computer.configure("cassandra.thrift.framed.size_mb", 1024);
         computer.configure("janusgraphmr.ioformat.conf.storage.cassandra.keyspace", keyspace);
         computer.configure("cassandra.input.partitioner.class", "org.apache.cassandra.dht.Murmur3Partitioner");
-        computer.configure("gremlin.hadoop.outputLocation", "output/1342398146");
+        computer.configure("gremlin.hadoop.outputLocation", AppConstants.GREMLIN_HADOOP_OUTPUT_LOCATION_VALUE_STANDALONE_SPARK);
         computer.configure("janusmr.ioformat.conf.storage.backend", "cassandra");
         computer.configure("janusmr.ioformat.cf-name", "edgestore");
         computer.configure("cassandra.input.columnfamily", "edgestore");
@@ -105,8 +106,17 @@ public class StandaloneJanus {
     public static JanusGraph loadGraphOfTheGodsGraph(Map<String, Object> configuration) {
         Configuration config = new MapConfiguration(configuration);
         JanusGraph graph = JanusGraphFactory.open(config);
-        GraphOfTheGodsFactory.loadWithoutMixedIndex(graph, false);
-        graph.newTransaction().commit();
+        JanusGraphTransaction tx = graph.newTransaction();
+//        GraphOfTheGodsFactory.loadWithoutMixedIndex(graph, false);
+        tx.commit();
+        return graph;
+    }
+
+    public static JanusGraph loadGraphOfTheGodsGraph2(Map<String, Object> configuration) {
+        Configuration config = new MapConfiguration(configuration);
+        JanusGraph graph = JanusGraphFactory.open(config);
+        JanusGraphTransaction tx = graph.newTransaction();
+        tx.commit();
         return graph;
     }
 //
