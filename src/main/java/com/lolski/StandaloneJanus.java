fff1f5315a107248a6d9e60a44a9fe2ee628c651
Ganeshwara Herawan Hananda <lolski@Ganeshwaras-MacBook-Pro.local>
2017-11-14 18:39:50 +0000

    standalone janus wip

--- src/main/java/com/lolski/StandaloneJanus.java
+++ src/main/java/com/lolski/StandaloneJanus.java
@@ -4,6 +4,7 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration2.ConfigurationConverter;
 import org.apache.commons.configuration.MapConfiguration;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.tinkerpop.gremlin.hadoop.Constants;
 import org.apache.tinkerpop.gremlin.hadoop.structure.HadoopGraph;
 import org.apache.tinkerpop.gremlin.process.computer.GraphComputer;
 import org.apache.tinkerpop.gremlin.spark.process.computer.SparkGraphComputer;
@@ -18,7 +19,7 @@ import java.util.Map;
 import java.util.UUID;
 
 public class StandaloneJanus {
-    public static final String keyspace = generateUniqueKeyspaceName();
+    public static final String keyspace = "janusgraph";
     public static Pair<Graph, GraphComputer> newStandaloneJanusSparkComputer() {
         Map<String, Object> janusConfig = newStandaloneJanusConfigurations();
         loadGraphOfTheGodsGraph(janusConfig);
@@ -32,10 +33,34 @@ public class StandaloneJanus {
 
     public static Map<String, Object> newStandaloneJanusConfigurations() {
         Map<String, Object> map = new HashMap<>();
-        map.put(AppConstants.STORAGE_HOSTNAME, "localhost");
+//        map.put(AppConstants.STORAGE_HOSTNAME, "localhost");
+//        map.put(AppConstants.STORAGE_BACKEND, "cassandra");
+//        map.put(AppConstants.STORAGE_CASSANDRA_KEYSPACE, keyspace);
+//        map.put(AppConstants.GREMLIN_GRAPH, AppConstants.GREMLIN_GRAPH_VALUE_HADOOP);
+//        map.put(Constants.GREMLIN_HADOOP_INPUT_LOCATION, null);
+        
+        map.put("cassandra.input.keyspace", keyspace);
+        map.put("gremlin.hadoop.jarsInDistributedCache", true);
+        map.put("cassandra.input.predicate", "0c00020b0001000000000b000200000000020003000800047fffffff0000");
+        map.put("janusgraphmr.ioformat.cf-name", "edgestore");
+        map.put("janusgraphmr.ioformat.conf.storage.hostname", "localhost");
+        map.put("cassandra.thrift.framed.size_mb", 1024);
+        map.put("janusgraphmr.ioformat.conf.storage.cassandra.keyspace", keyspace);
+        map.put("cassandra.input.partitioner.class", "org.apache.cassandra.dht.Murmur3Partitioner");
+        map.put("gremlin.hadoop.outputLocation", "output/1342398146");
+        map.put("janusmr.ioformat.conf.storage.backend", "cassandra");
+        map.put("janusmr.ioformat.cf-name", "edgestore");
+        map.put("cassandra.input.columnfamily", "edgestore");
+        map.put("janusmr.ioformat.conf.storage.cassandra.keyspace", keyspace);
+        map.put("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        map.put("gremlin.graph", "org.apache.tinkerpop.gremlin.hadoop.structure.HadoopGraph");
+        map.put("gremlin.hadoop.graphWriter", "org.apache.tinkerpop.gremlin.hadoop.structure.io.gryo.GryoOutputFormat");
+        map.put("storage.hostname", "localhost");
+        map.put("janusgraphmr.ioformat.conf.storage.backend", "cassandra");
+        map.put("janusmr.ioformat.conf.storage.hostname", "localhost");
+        map.put("gremlin.hadoop.graphReader", "org.janusgraph.hadoop.formats.cassandra.CassandraInputFormat");
+        map.put("gremlin.hadoop.inputLocation", "none");
         map.put(AppConstants.STORAGE_BACKEND, "cassandra");
-        map.put(AppConstants.STORAGE_CASSANDRA_KEYSPACE, keyspace);
-        map.put(AppConstants.GREMLIN_GRAPH, AppConstants.GREMLIN_GRAPH_VALUE_HADOOP);
 
         return map;
     }
@@ -50,6 +75,29 @@ public class StandaloneJanus {
         SparkGraphComputer computer = graph.compute(SparkGraphComputer.class);
         computer.configure(AppConstants.SPARK_MASTER, AppConstants.SPARK_MASTER_VALUE_STANDALONE);
         computer.configure(AppConstants.SPARK_SERIALIZER, AppConstants.SPARK_SERIALIZER_VALUE);
+
+        computer.configure("cassandra.input.keyspace", keyspace);
+        computer.configure("gremlin.hadoop.jarsInDistributedCache", true);
+        computer.configure("cassandra.input.predicate", "0c00020b0001000000000b000200000000020003000800047fffffff0000");
+        computer.configure("janusgraphmr.ioformat.cf-name", "edgestore");
+        computer.configure("janusgraphmr.ioformat.conf.storage.hostname", "localhost");
+        computer.configure("cassandra.thrift.framed.size_mb", 1024);
+        computer.configure("janusgraphmr.ioformat.conf.storage.cassandra.keyspace", keyspace);
+        computer.configure("cassandra.input.partitioner.class", "org.apache.cassandra.dht.Murmur3Partitioner");
+        computer.configure("gremlin.hadoop.outputLocation", "output/1342398146");
+        computer.configure("janusmr.ioformat.conf.storage.backend", "cassandra");
+        computer.configure("janusmr.ioformat.cf-name", "edgestore");
+        computer.configure("cassandra.input.columnfamily", "edgestore");
+        computer.configure("janusmr.ioformat.conf.storage.cassandra.keyspace", keyspace);
+        computer.configure("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        computer.configure("gremlin.graph", "org.apache.tinkerpop.gremlin.hadoop.structure.HadoopGraph");
+        computer.configure("gremlin.hadoop.graphWriter", "org.apache.tinkerpop.gremlin.hadoop.structure.io.gryo.GryoOutputFormat");
+        computer.configure("storage.hostname", "localhost");
+        computer.configure("janusgraphmr.ioformat.conf.storage.backend", "cassandra");
+        computer.configure("janusmr.ioformat.conf.storage.hostname", "localhost");
+        computer.configure("gremlin.hadoop.graphReader", "org.janusgraph.hadoop.formats.cassandra.CassandraInputFormat");
+        computer.configure("gremlin.hadoop.inputLocation", "none");
+        computer.configure(AppConstants.STORAGE_BACKEND, "cassandra");
 
         return computer;
     }
--- src/test/java/com/lolski/MainTest.java
+++ src/test/java/com/lolski/MainTest.java
@@ -1,38 +1,60 @@
 package com.lolski;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.tinkerpop.gremlin.process.computer.ComputerResult;
+import org.apache.tinkerpop.gremlin.process.computer.GraphComputer;
+import org.apache.tinkerpop.gremlin.process.computer.clustering.peerpressure.ClusterPopulationMapReduce;
+import org.apache.tinkerpop.gremlin.process.computer.clustering.peerpressure.PeerPressureVertexProgram;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static com.lolski.Helpers.p;
 
 /**
  * Unit test for simple Main.
  */
-public class MainTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MainTest(String testName )
-    {
-        super( testName );
+public class MainTest {
+    public void a() throws InterruptedException, ExecutionException {
+        Pair<Graph, GraphComputer> graphAndGraphComputer = LocalKryo.newLocalKryoSparkComputer();
+
+        Graph graph = graphAndGraphComputer.getLeft();
+        GraphComputer graphComputer = graphAndGraphComputer.getRight();
+
+        p("--- PROGRAM STARTING --- ");
+        graphComputer.program(PeerPressureVertexProgram.build().create(graph)).mapReduce(ClusterPopulationMapReduce.build().create());
+        Future<ComputerResult> work = graphComputer.submit();
+
+        p(" result =  " + work.get().memory().get("clusterPopulation"));
+        p("--- PROGRAM ENDED --- ");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MainTest.class );
+    public void b() throws InterruptedException, ExecutionException {
+        Pair<Graph, GraphComputer> graphAndGraphComputer = StandaloneKryo.newStandaloneKryoSparkComputer();
+
+        Graph graph = graphAndGraphComputer.getLeft();
+        GraphComputer graphComputer = graphAndGraphComputer.getRight();
+
+        p("--- PROGRAM STARTING --- ");
+        graphComputer.program(PeerPressureVertexProgram.build().create(graph)).mapReduce(ClusterPopulationMapReduce.build().create());
+        Future<ComputerResult> work = graphComputer.submit();
+
+        p(" result =  " + work.get().memory().get("clusterPopulation"));
+        p("--- PROGRAM ENDED --- ");
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void c() throws InterruptedException, ExecutionException {
+        Pair<Graph, GraphComputer> graphAndGraphComputer = StandaloneJanus.newStandaloneJanusSparkComputer();
+
+        Graph graph = graphAndGraphComputer.getLeft();
+        GraphComputer graphComputer = graphAndGraphComputer.getRight();
+
+        p("--- PROGRAM STARTING --- ");
+        graphComputer.program(PeerPressureVertexProgram.build().create(graph)).mapReduce(ClusterPopulationMapReduce.build().create());
+        Future<ComputerResult> work = graphComputer.submit();
+
+        p(" result =  " + work.get().memory().get("clusterPopulation"));
+        p("--- PROGRAM ENDED --- ");
     }
 }
